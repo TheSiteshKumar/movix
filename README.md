ce7fccfd0e72719dc2b377917542ffc8483e1663
Sitesh <thesiteshkumar@gmail.com>
2023-12-30 15:03:29 +0530

    mobile menu

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 01:19:20  
+02:32:41 monile menu complete not search icon
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import "./style.scss";
 
-function Header() {
-  return <div>Header</div>;
-}
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
+const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+    </header>
+  );
+};
 
 export default Header;
